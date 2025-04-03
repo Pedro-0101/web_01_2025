0ad130c1db68a39b27fd1b7fdaa48cc9a7dea40e
pedro-0101m <pedro.tiete@outlook.com>
2025-04-03 20:27:20 -0300

    Finalizado exe2 lab05

---
+++ LAB_05_Funcoes/src/exe_2.ts
@@ -0,0 +1,10 @@
+let mult = function(num1: number, num2: number):number{
+
+    return num1 * num2;
+
+}
+
+let mult1 = (num1: number, num2: number) => num1 * num2;
+
+console.log(mult(3, 2));
+console.log(mult1(3, 2));
