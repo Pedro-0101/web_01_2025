c61663025d8e29e1db9bf627eb9f2c353d186eaa
pedro-0101m <pedro.tiete@outlook.com>
2025-04-03 20:24:21 -0300

    finalizado exe1 lab05

---
+++ LAB_05_Funcoes/src/exe_1.ts
@@ -0,0 +1,10 @@
+let maiuscula = function(palavra: string):string{
+
+    return palavra.toUpperCase();
+
+}
+
+let maiuscula1 = (palavra: string) => palavra.toUpperCase();
+
+console.log(maiuscula("Teste"));
+console.log(maiuscula1("Teste"));
